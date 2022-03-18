0becee698a8da2b5bcb7b8a8678bf29b255e2326
alexx-potter458 <alexx.potter458@gmail.com>
2022-03-18 18:36:34 +0200

    ...

--- src/main/java/utils/Constants.java
+++ src/main/java/utils/Constants.java
@@ -2,7 +2,7 @@ package utils;
 
 public class Constants {
     public static final String gameName = "Hero Game ⚡";
-    public static final String loadingScreenStudioText = "POTTER STUDIOS\n      PRESENTS";
+    public static final String loadingScreenStudioText = "POTTER NET\n   PRESENTS";
     public static final String gameTitleCaps = "HERO GAME";
     public static final String startButton = "Start";
     public static final String settingsButton = "Settings";
